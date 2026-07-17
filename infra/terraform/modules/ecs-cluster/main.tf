be9565a45848ddee9fa2f83753df17bb171641f1
newgirok <newgirok@gmail.com>
2026-07-17 23:52:32 +0900

    refactor(infra): ecs-cluster Terraform 모듈 삭제 (미사용)

--- infra/terraform/modules/ecs-cluster/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecs_cluster" "cluster" {
-  name = var.cluster_name
-
-  # Container Insights 비활성화 (비용 절감)
-  setting {
-    name  = "containerInsights"
-    value = "disabled"
-  }
-}
--- infra/terraform/modules/ecs-cluster/outputs.tf
+++
@@ -1,2 +0,0 @@
-output "cluster_arn"  { value = aws_ecs_cluster.cluster.arn }
-output "cluster_name" { value = aws_ecs_cluster.cluster.name }
